6f1fb7c0507ea1bab74353624240ffa74fbdd1a4
Jesús Miguel Benito Calzada <beni0888@hotmail.com>
2016-02-29 13:26:15 +0100

    Remove unnecessary use of leading slash when intanciating some classes in the front script

--- src/twitterbis.php
+++ src/twitterbis.php
@@ -3,18 +3,18 @@
 require __DIR__.'/../vendor/autoload.php';
 
 $ioHandler = new TwitterBis\IO\StandardIOHandler();
-$messageList = new \TwitterBis\DataStructure\InMemoryMessageList(new \TwitterBis\DataStructure\InMemoryReversedSortedList());
-$userList = new \TwitterBis\DataStructure\InMemoryUserSet();
-$commandParser = new \TwitterBis\Application\Command\Parser($ioHandler, $userList, $messageList);
+$messageList = new TwitterBis\DataStructure\InMemoryMessageList(new \TwitterBis\DataStructure\InMemoryReversedSortedList());
+$userList = new TwitterBis\DataStructure\InMemoryUserSet();
+$commandParser = new TwitterBis\Application\Command\Parser($ioHandler, $userList, $messageList);
 
-$app = new \TwitterBis\Application\Application($ioHandler, $userList, $messageList);
+$app = new TwitterBis\Application\Application($ioHandler, $userList, $messageList);
 $app->loadUsers();
 
-while (\TwitterBis\Application\Application::EXIT_COMMAND !== ($userInput = $app->readCommand())) {
+while (TwitterBis\Application\Application::EXIT_COMMAND !== ($userInput = $app->readCommand())) {
     try {
         $command = $commandParser->parse($userInput);
         $command->run();
-    } catch (\Exception $e) {
+    } catch (Exception $e) {
         $ioHandler->writeLine(sprintf('ERROR: %s', $e->getMessage()));
     }
 }
