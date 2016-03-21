fae1478f2e82a236f22553a87e3e3d514e593dd2
Jesús Miguel Benito Calzada <beni0888@hotmail.com>
2016-03-21 12:45:55 +0100

    Refactor WallCommand
    This commits contains several changes in the WallCommand class. It applies several refactor techniques to make the code
    more maintainable.

--- src/Application/Command/Command/WallCommand.php
+++ src/Application/Command/Command/WallCommand.php
@@ -11,6 +11,45 @@ class WallCommand extends AbstractCommand
     const NAME = 'WALL';
     const USER_ARGUMENT_OFFSET = 0;
 
+    private $timeFormatter;
+
+    private function compose(Message $message)
+    {
+        $timestamp = $message->getTimestamp();
+        $time = $this->format($timestamp);
+        $printPattern = '%s: %s %s';
+
+        $composedMessage = sprintf($printPattern, $message->getAuthor(), $message->getText(), $time);
+        return $composedMessage;
+    }
+
+    private function format($timestamp)
+    {
+        $formatter = $this->retrieveTimeFormatter();
+        $time = $formatter->format($timestamp);
+        return $this->normalize($time);
+    }
+
+    private function normalize($time)
+    {
+        $result = "- ($time)";
+        if (empty($time)) $result = '';
+        return $result;
+    }
+
+    private function write(Message $message)
+    {
+        $this->ioHandler->writeLine($this->compose($message));
+    }
+
+    private function retrieveTimeFormatter()
+    {
+        if (is_null($this->timeFormatter)) {
+            $this->timeFormatter = new MessageTimeFormatter();
+        }
+        return $this->timeFormatter;
+    }
+
     /**
      * Run the command.
      * @return mixed
@@ -19,12 +58,10 @@ class WallCommand extends AbstractCommand
     {
         $user = $this->getUserByName($this->getArgument(self::USER_ARGUMENT_OFFSET));
         $userWall = new UserWall(new \IteratorIterator($this->messages), $user);
-        $timeFormatter = new MessageTimeFormatter();
 
         /** @var Message $message */
         foreach ($userWall as $message) {
-            $time = $timeFormatter->format($message->getTimestamp());
-            $this->ioHandler->writeLine(sprintf('%s: %s %s', $message->getAuthor(), $message->getText(), !empty($time) ? "- ($time)" : ''));
+            $this->write($message);
         }
     }
 }
